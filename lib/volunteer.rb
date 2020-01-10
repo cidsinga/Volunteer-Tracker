579f7d29eac118a412fc056c5afbf4e4ddf7da1f
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-10 12:10:58 -0800

    add red green test on self compare test on Volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,4 +7,8 @@ class Volunteer
     @id = attributes.fetch(:id)
     @project_id = attributes.fetch(:project_id)
   end
+
+def ==(self_to_compare)
+    (self.name() == self_to_compare.name()) && (self.project_id() == self_to_compare.project_id())
+  end
 end
