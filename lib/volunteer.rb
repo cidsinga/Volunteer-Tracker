d3e80e33241bb739f09175597f87c51d1101ddba
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-10 11:54:56 -0800

    add red green test on project_id test on Volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,9 +1,10 @@
 class Volunteer
-  attr_accessor :name
+  attr_accessor :name, :project_id
   attr_reader :id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
   end
 end
