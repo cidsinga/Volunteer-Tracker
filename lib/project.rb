b1bb8840dea020dcb96635487f47ffe46c69761a
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-10 11:34:28 -0800

    add passing test for self compare

--- lib/project.rb
+++ lib/project.rb
@@ -7,7 +7,14 @@ class Project
     @id = attributes.fetch(:id)
   end
 
-  # def title()
-  #   project = DB.exec("SELECT * title FROM projects;").first()
-  # end
+  def save()
+    new_id = DB.exec("INSERT INTO projects (title) VALUEs ('#{@title}') RETURNING id; ")
+    @id = new_id.first().fetch("id").to_i
+  end
+
+  def ==(self_compare)
+    self.title == self_compare.title ? true : false
+  end
+
+
 end
