be95d8baa761f1cbed1e843b11edc193423a2010
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-10 16:34:30 -0800

    add beginnings of front end

--- app.rb
+++ app.rb
@@ -5,8 +5,24 @@ require('./lib/project')
 require('./lib/volunteer')
 require('pg')
 
-DB = PG.connect({:dbname => "volunteer_tracker"})
+DB = PG.connect({:dbname => "new_volunteer_tracker"})
 
 get('/') do
-  erb(:index)
+  erb(:projects)
+end
+
+get('/projects') do
+  #will show a list of projects
+end
+
+post('/projects') do
+  #take us to a page for adding new projects
+  title = params[:title]
+  if title =~ /^[a-z0-9 \_ \-]*$/
+    project = Project.new({:title => title, :id => nil})
+    project.save()
+    erb(:projects)
+end
+
+
 end
--- lib/project.rb
+++ lib/project.rb
@@ -16,42 +16,38 @@ class Project
     self.title == self_compare.title ? true : false
   end
 
-
   def self.all()
-  from_db_projects = DB.exec("SELECT * FROM projects;")
-  projects = []
-  from_db_projects.each do |project|
-    title = project.fetch("title")
-    id = project.fetch("id").to_i
-    projects.push(Project.new({:title => title, :id => id}))
+    from_db_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    from_db_projects.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
   end
-  projects
-end
 
-def save()
-  results = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}')RETURNING id;").first
-  @id = results.fetch("id").to_i
-end
+  def save()
+    results = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}')RETURNING id;").first
+    @id = results.fetch("id").to_i
+  end
 
-def self.find(id)
-  returned_projects = DB.exec("SELECT * FROM projects WHERE id = #{id};").first()
-  title = returned_projects.fetch("title")
-  project = Project.new({:title => title, :id => id})
-end
+  def self.find(id)
+    returned_projects = DB.exec("SELECT * FROM projects WHERE id = #{id};").first()
+    title = returned_projects.fetch("title")
+    project = Project.new({:title => title, :id => id})
+  end
 
-def volunteers
-  Volunteer.find_by_project(self.id)
-end
+  def volunteers
+    Volunteer.find_by_project(self.id)
+  end
 
-def update(attributes)
-  @title = attributes.fetch(:title)
-  DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
-end
+  def update(attributes)
+    @title = attributes.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
 
-def delete()
-  DB.exec("DELETE FROM projects WHERE id = #{@id};")
-
-end
-
-
+  def delete()
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+  end
 end
